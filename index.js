b6932730f6df33805344789d885b6909ef96a6fe
ryeqb <me@ryeqb.xyz>
2020-06-04 18:52:03 +0200

    Implement routine to generate a settings file dynamically

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const Discord = require("discord.js");
 const fs = require('fs');
 const rl = require("readline-sync");
 const request = require("request");
-const settings = require("./settings.json");
 const client = new Discord.Client();
 
 function sanitize(string) {
@@ -55,7 +54,7 @@ function dump(client, inputID) {
 }
 
 function main(client){
-	console.log("Which server id would you like to dump?...");
+	console.log("[?] Which server id would you like to dump?...");
 	let input = rl.prompt();
 
 	if(input == "0") {
@@ -71,11 +70,42 @@ function main(client){
 	return;
 }
 
+function init(client){
+	console.log("[!] Thank you for using this emoji dumper!");
+
+	if(!fs.existsSync("./settings.json")){
+		console.log("[!] Seems as though you are missing a settings file...");
+		console.log("[!] Please enter your Discord token to proceed...");
+		let token = rl.prompt();
+
+		let json = {
+			"client": {
+				"token": `${token}`
+			}
+		}
+
+		fs.appendFile("./settings.json", JSON.stringify(json, null, "\t"), (err) => {
+			if(err)
+				console.error(err);
+
+			console.log("[!] Settings successfully saved!\n\n");
+
+			let data = JSON.parse(fs.readFileSync("./settings.json", {encoding: "utf8"}));
+			client.login(data.client.token);
+		});
+
+		return;
+	}
+
+	let data = JSON.parse(fs.readFileSync("./settings.json", {encoding: "utf8"}));
+	client.login(data.client.token);
+}
+
+init(client);
+
 client.on('ready', () => {
 	console.log(`[!] Connected to gateway, logged in as ${client.user.username}!`);
 	console.log("[!] Welcome to emojidumper!  [  Enter 0 to exit!  ]");
 
 	main(client);
 });
-
-client.login(settings.client.token);
